884314054c982bf7048c118109815bfb30884b40
Agustin Fernandez <af.agusfernandez02@gmail.com>
2021-08-26 20:05:17 -0300

    add google analytics

--- src/index.js
+++ src/index.js
@@ -4,6 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDRjCnMiE3hJOv-tjOViNO44QVKXFciCSs",
+  authDomain: "todolist-99f59.firebaseapp.com",
+  projectId: "todolist-99f59",
+  storageBucket: "todolist-99f59.appspot.com",
+  messagingSenderId: "936252916382",
+  appId: "1:936252916382:web:c7dd14492f8df0f5fbf18f",
+  measurementId: "G-F2SXMN3NG7"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -14,4 +30,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(); 
